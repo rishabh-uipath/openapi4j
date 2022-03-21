a0e86ce2558d413cf8860032005610d7bd3cdd39
Rishabh Ag <rishabh@Rishabhs-MacBook-Pro.local>
2022-03-20 19:56:41 -0500

    bug: parser failing validation where key contain colon

--- openapi-parser/src/main/java/org/openapi4j/parser/validation/v3/Regexes.java
+++ openapi-parser/src/main/java/org/openapi4j/parser/validation/v3/Regexes.java
@@ -9,7 +9,7 @@ class Regexes {
   static final Pattern EXT_REGEX = Pattern.compile("x-.+");
   static final Pattern NOEXT_REGEX = Pattern.compile("(?!x-).*");
   static final Pattern NAME_REGEX = Pattern.compile("[a-zA-Z0-9._-]+");
-  static final Pattern NOEXT_NAME_REGEX = Pattern.compile("(?!x-)[a-zA-Z0-9._-]+");
+  static final Pattern NOEXT_NAME_REGEX = Pattern.compile("(?!x-)[a-zA-Z0-9:._-]+");
   static final Pattern METHOD_REGEX = Pattern.compile("get|put|post|delete|options|head|patch|trace");
   static final Pattern PARAM_IN_REGEX = Pattern.compile("path|query|header|cookie");
   static final Pattern STYLE_REGEX = Pattern.compile("matrix|label|form|simple|spaceDelimited|pipeDelimited|deepObject");
